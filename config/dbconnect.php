bdb0e27dd3170b652baed739f17bae24aa26b9de
wahujos <40623396+wahujos@users.noreply.github.com>
2021-08-01 12:38:51 +0300

    Update dbconnect.php

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,9 +1,18 @@
 <?php
   
+//dev environment
+/*
   define('DB_HOST', 'localhost');
   define('DB_USER','root');
   define('DB_PASS', '');
   define('DB_NAME', 'notes');
+  
+  */
+
+  define('DB_HOST', 'remotemysql.com');
+  define('DB_USER','VrGkxuBVJo');
+  define('DB_PASS', 'NBwGsWBGAn');
+  define('DB_NAME', 'VrGkxuBVJo');
 
   $link = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
